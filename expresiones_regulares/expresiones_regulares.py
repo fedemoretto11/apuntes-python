681d4ace240df5762ebeb4db950f91d04e3abd72
fedemoretto11 <fedemoretto94@gmail.com>
2023-01-25 17:00:55 -0300

    Apuntes de Python, curso DALTO

--- expresiones_regulares/expresiones_regulares.py
+++ expresiones_regulares/expresiones_regulares.py
@@ -11,5 +11,13 @@ Y esta la 3ra pete'''
 
 # Armado de cadena que busque numero, seguido de punto y seguido espacio
 
-resultado = re.findall(f"\d{4}", texto)
-print(resultado)
+# resultado = re.findall(f"\d{4}", texto)
+# print(resultado)
+
+
+texto = "Hola, mi numero de telefono es: 2268 510595 2268 518438"
+
+pattern = r'\d{4}\s\d{6}'
+
+reemplazo = re.sub(pattern,"(Numero oculto)", texto)
+print(reemplazo)
